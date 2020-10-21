4c280632bce8a74140fe82898bd7191d0fdf5262
januszpolak <janusz.polak@interia.pl>
2020-10-21 11:43:16 +0200

    add more features

--- Program.cs
+++ Program.cs
@@ -16,8 +16,14 @@ namespace Weather
             using (WebClient client = new WebClient())
 
             results = client.DownloadString("http://api.openweathermap.org/data/2.5/weather?q=cracow&appid=085f4e9a1bb01af375c061ffa74b3886&units=metric");
-            dynamic jo = JObject.Parse(results);
-            Console.WriteLine($"Temperature: " + jo.main.temp + " Celcius");
+            dynamic weatherResults = JObject.Parse(results);
+            Console.WriteLine($"Temperature: " + weatherResults.main.temp + " Celcius");
+            Console.WriteLine($"Pressure: " + weatherResults.main.pressure + " hPa");
+            Console.WriteLine($"Humidity: " + weatherResults.main.humidity + " %");
+            Console.WriteLine($"Visibility: " + weatherResults.visibility + " m");
+            Console.WriteLine($"Description: " + weatherResults.weather[0].description);
+
+            Console.ReadLine();
 
         }
 
