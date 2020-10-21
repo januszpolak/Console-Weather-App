4501fc74c39408ce9883d76c3f25c18a505b4d6d
januszpolak <janusz.polak@interia.pl>
2020-10-21 10:55:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+
+namespace Weather
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            string results;
+            Console.WriteLine("Current weather in Cracow Poland");
+
+            using (WebClient client = new WebClient())
+
+            results = client.DownloadString("http://api.openweathermap.org/data/2.5/weather?q=cracow&appid=085f4e9a1bb01af375c061ffa74b3886&units=metric");
+            dynamic jo = JObject.Parse(results);
+            Console.WriteLine($"Temperature: " + jo.main.temp + " Celcius");
+
+        }
+
+       
+    }
+}
